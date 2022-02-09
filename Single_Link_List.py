546b92f3d7002e86f0e25ef68213390f74d34440
M M Aftab Chowdhury <mmaftab.chowdhury@cgi.com>
2022-02-09 21:14:47 +0100

    Single_Link_List: add method for insert head

--- Single_Link_List.py
+++ Single_Link_List.py
@@ -2,3 +2,13 @@ class Node:
     def __init__(self, value):
         self.node_value = value
         self.next = None
+
+
+class SingleLinkList:
+    def __init__(self):
+        self.head = None
+
+    def add_head(self, value):
+        node = Node(value)
+        node.next = self.head
+        self.head = node.node_value
