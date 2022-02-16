ae72e3b6ac33e462461aa474b0f45813f1575296
M M Aftab Chowdhury <mmaftab.chowdhury@cgi.com>
2022-02-16 22:56:36 +0100

    Doubly_Link_List: implement add_last_node and print_reverse_list methods

--- Doubly_Link_List.py
+++ Doubly_Link_List.py
@@ -16,9 +16,22 @@ class DoublyLinkList:
         if self.head is None:
             self.head = node
             self.tail = node
+            self.size += 1
         else:
             node.next = self.head
             self.head = node
+            self.size += 1
+
+    def add_last_node(self, value):
+        node = Node(value)
+        if self.head is None:
+            self.head = node
+            self.tail = node
+            self.size += 1
+        else:
+            self.tail.next = node
+            node.previous = self.tail
+            self.tail = node
             self.size += 1
 
     def print_values(self):
@@ -27,7 +40,19 @@ class DoublyLinkList:
             print('Node: ', traverse_node.node_value)
             traverse_node = traverse_node.next
 
+    def print_reverse_values(self):
+        traverse_node = self.tail
+        while traverse_node:
+            print('Node: ', traverse_node.node_value)
+            traverse_node = traverse_node.previous
+
 
 objDoubly_Link_List = DoublyLinkList()
 objDoubly_Link_List.add_head(1)
+objDoubly_Link_List.add_last_node(2)
+objDoubly_Link_List.add_last_node(3)
+objDoubly_Link_List.add_last_node(4)
+print('Forward List')
 objDoubly_Link_List.print_values()
+print('Reverse List')
+objDoubly_Link_List.print_reverse_values()
