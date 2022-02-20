91aa8445e33035f46cf740ca7e47d4e25c98c0ed
M M Aftab Chowdhury <mmaftab.chowdhury@cgi.com>
2022-02-20 22:16:27 +0100

    Doubly_Link_List: implement delete_node method

--- Doubly_Link_List.py
+++ Doubly_Link_List.py
@@ -54,6 +54,26 @@ class DoublyLinkList:
                 else:
                     traverse_node = traverse_node.next
 
+    def delete_node(self, value):
+        traverse_node = self.head
+        while traverse_node:
+            if traverse_node.node_value == value:
+                if traverse_node.node_value == self.head.node_value:
+                    self.head = traverse_node.next
+                    self.size -= 1
+                    break
+                elif traverse_node.node_value == self.tail.node_value:
+                    self.tail = traverse_node.previous
+                    self.size -= 1
+                    break
+                else:
+                    previous_node.next = traverse_node.next
+                    self.size -= 1
+                    break
+            else:
+                previous_node = traverse_node
+                traverse_node = traverse_node.next
+
     def print_values(self):
         traverse_node = self.head
         while traverse_node:
@@ -73,6 +93,7 @@ objDoubly_Link_List.add_last_node(2)
 objDoubly_Link_List.add_last_node(3)
 objDoubly_Link_List.add_last_node(4)
 objDoubly_Link_List.add_after_specific_node(2, 20)
+objDoubly_Link_List.delete_node(20)
 print('Forward List')
 objDoubly_Link_List.print_values()
 print('Reverse List')
