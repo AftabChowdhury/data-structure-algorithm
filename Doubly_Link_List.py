814307d0afe2a5029729bdd45a3a55a5f62af538
M M Aftab Chowdhury <mmaftab.chowdhury@cgi.com>
2022-02-20 20:44:47 +0100

    Doubly_Link_List: implement method add_after_specific_node

--- Doubly_Link_List.py
+++ Doubly_Link_List.py
@@ -34,6 +34,26 @@ class DoublyLinkList:
             self.tail = node
             self.size += 1
 
+    def add_after_specific_node(self, specific_node_value, value):
+        node = Node(value)
+        if self.head is None:
+            self.head = node
+            self.tail = node
+            self.size += 1
+        else:
+            traverse_node = self.head
+            while traverse_node:
+                if traverse_node.node_value == specific_node_value:
+                    node.next = traverse_node.next
+                    traverse_node.next = node
+                    node.previous = traverse_node
+                    self.size += 1
+                    if node.next:
+                        node.next.previous = node
+                    break
+                else:
+                    traverse_node = traverse_node.next
+
     def print_values(self):
         traverse_node = self.head
         while traverse_node:
@@ -52,6 +72,7 @@ objDoubly_Link_List.add_head(1)
 objDoubly_Link_List.add_last_node(2)
 objDoubly_Link_List.add_last_node(3)
 objDoubly_Link_List.add_last_node(4)
+objDoubly_Link_List.add_after_specific_node(2, 20)
 print('Forward List')
 objDoubly_Link_List.print_values()
 print('Reverse List')
