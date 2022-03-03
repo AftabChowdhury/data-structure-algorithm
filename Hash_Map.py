662a2252c58fed7a59ef61191dd664d23d794654
M M Aftab Chowdhury <mmaftab.chowdhury@cgi.com>
2022-03-03 23:35:54 +0100

    Hash_Map: create a hash map from string and find the non repeating character from string

---
+++ Hash_Map.py
@@ -0,0 +1,22 @@
+def create_hash_map_from_string(string_name):
+    hash_map = {}
+    for character in string_name:
+        if hash_map.get(character) is None:
+            hash_map[character] = 1
+        else:
+            hash_map[character] = hash_map[character] + 1
+    return hash_map
+
+
+hash_map_from_string = create_hash_map_from_string('aftab')
+print(hash_map_from_string)
+
+
+def get_non_repeated_first_character(created_hash_map):
+    for key, value in created_hash_map.items():
+        if value == 1:
+            return key
+
+
+non_repeated_first_character = get_non_repeated_first_character(hash_map_from_string)
+print(non_repeated_first_character)
