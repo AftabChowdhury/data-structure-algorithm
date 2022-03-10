2761b8398a4457dac8402dbcb7d3f6ccb29985a0
M M Aftab Chowdhury <mmaftab.chowdhury@cgi.com>
2022-03-10 23:47:08 +0100

    Binary_Search_Tree: implement search method

--- BInary_Search_Tree.py
+++ BInary_Search_Tree.py
@@ -19,6 +19,21 @@ class Node:
             else:
                 self.right_node.add_new_node(value)
 
+    def search_node(self, value):
+        if value == self.node_value:
+            print('Node found, Node: ', value)
+            return
+        elif value < self.node_value:
+            if self.left_node is None:
+                print('Node not found, Node: ', value)
+                return
+            self.left_node.search_node(value)
+        elif value > self.node_value:
+            if self.right_node is None:
+                print('Node not found, Node: ', value)
+                return
+            self.right_node.search_node(value)
+
     def print_tree(self):
         if self.left_node:
             self.left_node.print_tree()
@@ -26,9 +41,13 @@ class Node:
         if self.right_node:
             self.right_node.print_tree()
 
-root = Node(1)
-root.add_new_node(2)
-root.add_new_node(3)
-root.add_new_node(1)
-root.print_tree()
 
+root = Node(3)
+root.add_new_node(2)
+root.add_new_node(4)
+root.add_new_node(1)
+root.add_new_node(5)
+root.add_new_node(4)
+root.print_tree()
+root.search_node(1)
+root.search_node(10)
