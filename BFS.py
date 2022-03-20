140746c0e5a08e7053b6ce613036ff898ee0562f
M M Aftab Chowdhury <mmaftab.chowdhury@cgi.com>
2022-03-20 22:04:54 +0100

    BFS: rename variable

--- BFS.py
+++ BFS.py
@@ -16,8 +16,8 @@ def bfs(visited, graph, node):
     queue.append(node)
 
     while queue:
-        s = queue.pop(0)
-        print(s, end=' ')
+        current_node = queue.pop(0)
+        print(current_node, end=' ')
         for adjacent_node in graph:
             if adjacent_node not in visited:
                 visited.add(adjacent_node)
